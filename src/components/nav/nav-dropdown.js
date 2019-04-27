6ca903e22526197f67b2ab365375b51aa5c08257
JongMin (Home) <jongm711@gmail.com>
2019-04-27 17:25:35 +0900

    nav 추가

---
+++ src/components/nav/nav-dropdown.js
@@ -0,0 +1,42 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import './nav.css'
+
+class Dropdown extends Component {
+  render() {
+    return (
+      <li key={this.props.mainTitle+"_key"} className="nav-item dropdown">
+        <button
+          className="nav-link dropdown-toggle dropdown-item ButtonLink"
+          id={"navbarDropdown" + this.props.id}
+          data-toggle="dropdown"
+          aria-haspopup="true"
+          aria-expanded="false"
+        >
+          {this.props.mainTitle}
+        </button>
+        <div
+          className="dropdown-menu"
+          aria-labelledby={"navbarDropdown" + this.props.id}
+        >
+          {this.props.subTitles.map(title => (
+            <Link key={title+"_d_item"} to={"/" + title} className="dropdown-item">
+              {title}
+            </Link>
+          ))}
+          {this.props.d_titles !== undefined && (
+            <div className="dropdown-divider" />
+          )}
+          {this.props.d_titles !== undefined &&
+            this.props.d_titles.map(title => (
+              <Link key={title+"_dd_item"} to={"/" + title} className="dropdown-item">
+                {title}
+              </Link>
+            ))}
+        </div>
+      </li>
+    );
+  }
+}
+
+export default Dropdown;
---
+++ src/components/nav/nav.js
@@ -0,0 +1,59 @@
+import React, { Component } from "react";
+import Dropdown from "./nav-dropdown";
+import { Link } from "react-router-dom";
+
+class Nav extends Component {
+  render() {
+    return (
+      <nav className="navbar navbar-expand-lg py-3 rounded" style={{"backgroundColor": "#FF6E00"}}>
+        <span className="navbar-brand text-white">HorangApple</span>
+        <button
+          className="navbar-toggler bg-white navbar-light ButtonLink rounded"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon" />
+        </button>
+
+        <div
+          key="navbar"
+          className="collapse navbar-collapse"
+          id="navbarSupportedContent"
+        >
+          <ul className="navbar-nav ml-auto bg-white rounded px-3">
+            <li className="nav-item ">
+              <Link className="nav-link dropdown-item" to="/">
+                Home
+              </Link>
+            </li>
+            <Dropdown
+              id={1}
+              mainTitle={"Python"}
+              subTitles={["Python", "Django"]}
+            />
+            <Dropdown
+              id={2}
+              mainTitle={"JavaScript"}
+              subTitles={["Javascript", "React"]}
+            />
+            <Dropdown
+              id={3}
+              mainTitle={"Etc"}
+              subTitles={["Algorithm", "Tool"]}
+            />
+          </ul>
+          {/* <form className="form-inline my-2 my-lg-0">
+            <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
+            <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+          </form> */}
+        </div>
+      </nav>
+    );
+  }
+}
+
+export default Nav;
